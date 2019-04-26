915bda554bbdd2fdd718f8f9531217e276947036
Yuki HAMADA <s1f101700192@iniad.org>
2019-04-26 10:57:49 +0900

    insert space between [ to f

--- AutoInstall.sh
+++ AutoInstall.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 LOOK_FILE="~/.vimrc"
-if [-f ${LOOK_FILE} ]; then
+
+if [ -f ${LOOK_FILE} ]; then
   echo "renameing current .vimrc to .oldvimrc"
   mv ~/.vimrc ~/.oldvimrc
 fi
 
   echo "copying .vimrc from remote repositoly to ~/"
   cp ./.vimrc ~/
+  echo "Finished..."
